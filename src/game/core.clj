305a56fd1f6f9f0eec12ca688830ff043baeffb2
akankshakutal <44019583+akankshakutal@users.noreply.github.com>
2020-01-14 16:32:43 +0530

    Complete console view part

--- src/game/core.clj
+++ src/game/core.clj
@@ -10,32 +10,47 @@
     [1 5 9],
     [3 5 7]})
 
-(def game {:current #{} :opponent #{} :won false})
+(def game {:current {:moves #{} :symbol "X"} :opponent {:moves #{} :symbol "O"} :won false})
 
-(defn valid-move?
-  [input]
-  (<= 1 input 9))
+(defn valid-move? [input] (<= 1 input 9))
 
-(defn has-won?
-  [user-moves]
-  (some #(every? user-moves %) win-sets))
+(defn has-won? [user-moves] (some #(every? user-moves %) win-sets))
+
+(defn update-board
+  [player board]
+  (reduce #(assoc %1 (dec %2) (:symbol player)) board (:moves player)))
+
+(defn get-board
+  [{:keys [current opponent]}]
+  (->> (into [] (repeat 9 " "))
+       (update-board current)
+       (update-board opponent)))
+
+(defn print-board
+  [game]
+  (->> game
+       (get-board)
+       (partition 3)
+       (map #(clojure.string/join " | " %1))
+       (clojure.string/join "\n---------\n")
+       (println)))
 
 (defn make-move
   [user-input {:keys [current opponent]}]
-  (let [current (conj current user-input)]
-    (if (has-won? current)
-      (assoc game :current opponent :opponent current :won true)
-      (assoc game :current opponent :opponent current))))
+  (let [current (update current :moves conj user-input)
+        game (assoc game :current opponent :opponent current)]
+    (if (has-won? (:moves current)) (assoc game :won true) game)))
 
-(defn get-user-input
+(defn start
   []
-  (loop [input (read) game game]
-    (if (has-won? (:opponent (make-move input game)))
-      (println "You Won")
-      (do
-        (println "Enter Number :")
-        (if (valid-move? input)
-          (recur (read) (make-move input game))
-          (println "Invalid Move")
-          )
-        ))))
+  (do
+    (print-board game)
+    (loop [input (read) game game]
+      (if (:won game)
+        (println (str (:symbol (:opponent game)) " Won"))
+        (do
+          (print-board (make-move input game))
+          (println (str (:symbol (:current game)) " Enter Number :"))
+          (if (valid-move? input)
+            (recur (read) (make-move input game))
+            (println "Invalid Move")))))))
